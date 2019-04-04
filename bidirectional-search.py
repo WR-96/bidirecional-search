a2687785022c47c0b1c7d77adcf5764a7298799b
Williams Estrada <w.estrada.r@outlook.com>
2019-04-04 10:09:42 -0600

    Change example graph

--- bidirectional-search.py
+++ bidirectional-search.py
@@ -1,4 +1,10 @@
-example_graph = {0:[1,2], 1:[0,3,4], 3:[1], 4:[1], 2:[0,5,6], 5:[2], 6:[2]}
+example_graph = {
+        0:[2,3], 1:[4,5], 2:[0,6], 3:[0,6], 6:[2,3,8],
+        4:[1,7], 5:[1,7], 7:[4,5,8], 8:[6,7,9, 10],
+        9:[8,11,12], 11:[9,15], 12:[9,15],
+        10:[8,13, 14], 13:[10,16], 14:[10,16], 
+        15:[11,12], 16:[13,14]
+        }
 
 def bi_directional_search(graph, start, goal):
     # Check if start and goal are equal.
@@ -45,4 +51,4 @@ def bi_directional_search(graph, start, goal):
 
     return None
 
-print (bi_directional_search(example_graph,0,3))
+print (bi_directional_search(example_graph,0,16))
