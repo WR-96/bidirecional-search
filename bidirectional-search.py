f9973003b90af3e0aee7eb34986baf9d59fddabc
Williams Estrada <w.estrada.r@outlook.com>
2019-04-04 18:01:25 -0600

    More refactor idk

--- bidirectional-search.py
+++ bidirectional-search.py
@@ -7,45 +7,39 @@ example_graph = {
         }
 
 def bi_directional_search(graph, start, goal):
-    # Check if start and goal are equal.
+
     if start == goal:
         return [start]
-    # Get dictionary of currently active vertices with their corresponding paths.
+
     search_path = {start: [start], goal: [goal]}
-    # Vertices we have already examined.
     visited_vertices = set()
 
     while len(search_path) > 0:
+        unvisited_vertices = list(search_path.keys())
 
-        # Make a copy of active vertices so we can modify the original dictionary as we go.
-        active_vertices = list(search_path.keys())
-        for vertex in active_vertices:
-            # Get the path to where we are.
+        for vertex in unvisited_vertices:
+
             current_path = search_path[vertex]
-            # Record whether we started at start or goal.
-            origin = current_path[0]
-            # Check for new neighbours.
+            origin_vertex = current_path[0]
             current_neighbours = set(graph[vertex]) - visited_vertices
+            unvisited_neighbours = current_neighbours.intersection(unvisited_vertices) 
+
             # Check if our neighbours hit an active vertex
-            if len(current_neighbours.intersection(active_vertices)) > 0:
-                for meeting_vertex in current_neighbours.intersection(active_vertices):
-                    # Check the two paths didn't start at same place. If not, then we've got a path from start to goal.
-                    if origin != search_path[meeting_vertex][0]:
-                        # Reverse one of the paths.
+            if len(unvisited_neighbours) > 0:
+                for meeting_vertex in unvisited_neighbours:
+                    if origin_vertex != search_path[meeting_vertex][0]:
                         search_path[meeting_vertex].reverse()
-                        # return the combined results
                         return search_path[vertex] + search_path[meeting_vertex]
 
             # No hits, so check for new neighbours to extend our paths.
-            if len(set(current_neighbours) - visited_vertices - set(active_vertices))  == 0:
-                # If none, then remove the current path and record the endpoint as inactive.
+            if len(set(current_neighbours) - visited_vertices - set(unvisited_vertices))  == 0:
                 search_path.pop(vertex, None)
                 visited_vertices.add(vertex)
             else:
                 # Otherwise extend the paths, remove the previous one and update the inactive vertices.
-                for neighbour_vertex in current_neighbours - visited_vertices - set(active_vertices):
+                for neighbour_vertex in current_neighbours - visited_vertices - set(unvisited_vertices):
                     search_path[neighbour_vertex] = current_path + [neighbour_vertex]
-                    active_vertices.append(neighbour_vertex)
+                    unvisited_vertices.append(neighbour_vertex)
                 search_path.pop(vertex, None)
                 visited_vertices.add(vertex)
 
